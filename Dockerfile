88463d90037d434e90806736d5c036c5ea2fe692
Harman Singh Jaggi <harmanjaggs@gmail.com>
2026-01-05 01:21:35 +0530

    Revert "include tests"
    This reverts commit d0c665439e2da16f5a5a4e424ff7404029b6e491.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.9-slim
 
 WORKDIR /app
 
-# Install system-level dependencies
+# Install system-level dependencies needed by matplotlib
 RUN apt-get update && apt-get install -y \
     gcc \
     g++ \
@@ -16,11 +16,6 @@ COPY requirements.txt .
 RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the entire project
 COPY . .
-
-# --- NEW: Run tests during build ---
-# If these fail, the build stops here.
-RUN pytest tests/
 
 CMD ["python", "pipeline_runner.py"]
--- pipeline_runner.py
+++ pipeline_runner.py
@@ -6,21 +6,16 @@ SCRIPTS = [
     "scripts/store_parquet.py",
     "scripts/data_validation.py",
     "scripts/data_preparation.py",
-    "tests/test_data_preparation.py",
-    "tests/test_transformation.py",
     "scripts/feature_store.py",
     "scripts/feature_retreival_storage.py",
     "scripts/data_versioning.py",
-    "scripts/data_modeling.py",
-    "tests/test_model_training.py",
+    "scripts/data_modeling.py"
 ]
 
 def run(script):
     print(f"\n🚀 Running {script}")
-    command = ["pytest", script] if script.startswith("tests/") else ["python", script]
-    
     result = subprocess.run(
-        command,
+        ["python", script],
         check=True
     )
 
